d90bf0832832be75b39428431fed7e1239524bcf
Eloah Pucci <eloahpucci@gmail.com>
2019-05-29 18:36:59 -0300

    filme-form concluido e funcionando

--- src/app/filmes/filmes.routing.ts
+++ src/app/filmes/filmes.routing.ts
@@ -1,11 +1,12 @@
 import { Routes, RouterModule } from '@angular/router'
 import { FilmeListComponent } from './filme-list/filme-list.component';
+import { FilmeFormComponent } from './filme-form/filme-form.component';
 
 const FILMES_ROUTES: Routes = [
     {
         path: '',
-        component: FilmeListComponent
+        component: FilmeFormComponent
     }
 
 ];
-export const filmesRouting = RouterModule.forChild(FILMES_ROUTES);
+export const filmesRouting = RouterModule.forChild(FILMES_ROUTES);  
--- src/app/filmes/filmes.service.ts
+++ src/app/filmes/filmes.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Injectable, EventEmitter } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
 import { Filme } from './filme';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/catch';
-import { Observable } from 'rxjs/Observable';
 import { getAllDebugNodes } from '@angular/core/src/debug/debug_node';
 
 @Injectable()
@@ -13,8 +13,9 @@ export class FilmesService {
 
   private url: string = 'http://localhost:8080/filmes';
 
-  constructor(private http: Http) { }
+  filmesChanged = new EventEmitter<Observable<Filme[]>>();
 
+  constructor(private http: Http) { }
 
   getAll(): Observable<Filme[]> {
     return this.http.get(this.url)
@@ -27,6 +28,43 @@ export class FilmesService {
     let erro = error.message || 'Server error';
     console.error('Ocorreu um erro', error);
     return Observable.throw(erro);
+  }
+
+  add(filme: Filme) {
+    return this.http.post(this.url,JSON.stringify(filme),
+    {headers: this.getHeaders()})
+    .do(data => this.filmesChanged.emit(this.getAll()))
+    .catch(this.handleError);
+  }
+
+  remove(id: number) {
+    return this.http.delete(this.getUrl(id), {headers: this.getHeaders()})
+    .map(res => res.json())
+    .do(data => this.filmesChanged.emit(this.getAll()))
+    .catch(this.handleError);
+  }
+
+  private getHeaders() {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return headers;
+  }
+
+  private getUrl(id: number) {
+    return `${this.url}/${id}`;
+  }
+
+  update(filme: Filme) {
+    return this.http.put(this.url,JSON.stringify(filme),
+    {headers: this.getHeaders()})
+    .do(data => this.filmesChanged.emit(this.getAll()))
+    .catch(this.handleError);
+  }
+
+  get(id: number) {
+    return this.getAll()
+    .map((list: any) => list.find(filme => filme.codigo == id))
+    .catch(this.handleError);
   }
 }
 
